f90380f7b3c5f19a36cdbf2b16f2226387736ec1
Da-Me <damekim1206@gmail.com>
2016-02-16 10:34:35 -0500

    remove some bootstrap, add facts

---
+++ js/random_facts.js
@@ -0,0 +1,27 @@
+
+var randomFact = {
+  "facts": [
+    {"text":"I've raised a Black Bear, until he got too big."},
+    {"text":"I was once a train conductor, Choo Choo!"},
+    {"text":"I once slid across the hood of a cop car with a ski-mask on."},
+    {"text":"I trained my dog to catch fish."},
+    {"text":"I backpacked Europe for a summer."},
+    {"text":"I love swimming and ultimate frisbee!"},
+    {"text":"I had an American wedding and a traditional Korean style one."},
+    {"text":"I'm good freinds with season 8 American Idol winner Kris Allen."},
+    {"text":"Needs to open eyes wider,""was once written on my ballroom dancing test."}
+  ]
+};
+
+function generateRandomFact(first, last) {
+  return Math.floor(Math.random() * (last - first + 1)) + first;
+}
+
+randomFactIndex = generateRandomFact(1, 9) - 1;
+$("#fact-text").append( randomFact.facts[randomFactIndex].text );
+
+function onClick() {
+  randomFactIndex = generateRandomFact(1, 9) - 1;
+  $( "#fact-text" ).empty();
+  $( "#fact-text" ).prepend( randomFact.facts[randomFactIndex].text );
+}
